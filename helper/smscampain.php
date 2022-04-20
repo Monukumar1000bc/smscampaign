4535ee993ae823d063003fa531f12d25863bad74
monukumar <monu@cozyvision.com>
2022-04-20 12:42:07 +0530

    common logic for hide and show

--- helper/smscampain.php
+++ helper/smscampain.php
@@ -1,70 +1,80 @@
 <?php
+function data_phone()
+{
+    if (!empty($_GET['dogl-names'])) {
+        $selected = $_GET['dogl-names'];
 
+
+        $args = array(
+            'status' => array($selected),
+        );
+    
+    }
+   else {
+        $args = array();
+    }
+
+
+    $orders = wc_get_orders($args);
+
+    $temp_array = array();
+    foreach ($orders as $key => $order) {
+        $phone = $order->get_billing_phone();
+        $temp_array[] = $phone;
+    }
+
+    $shortdata = (array_unique($temp_array));
+    return $shortdata;
+    
+}
 
 function subscriber_page_smscampain()
 {
-global $shortdata;
+
 ?>
     <h2 style="text-align: center;">SMS CAMPAIGN</h2>
     <hr style="border:2px solid lightgray">
 
     <?php
+
     if (empty($_GET['dogl-names'])) { ?>
         <form action="" method="GET" for="dogl-names">
 
             <select name="dogl-names">
-                <option value="">All Customer</option>
+                <option value="">Slect Data</option>
+                <option value="shop_order">All Customer </option>
                 <option value="wc-processing">processing data</option>
                 <option value="wc-on-hold">on-hold data</option>
                 <option value="wc-cancelled">Cancelled data</option>
                 <option value="wc-complete">Completed data</option>
+               
             </select>
             <input type="hidden" name="page" value="all-smscampain">
             <input type="submit" name="submit" value="Search data">
 
         </form>
 
-    <?php
+        <?php
     }
 
     if (!empty($_REQUEST['submit']) && $_REQUEST['submit'] == 'Search data') {
-
-        if (!empty($_GET['dogl-names'])) {
-            $selected = $_GET['dogl-names'];
-
-
-            $args = array(
-                'status' => array($selected),
-            );
-        } else {
-            $args = array();
-        }
-
-
-        $orders = wc_get_orders($args);
-
-        $temp_array = array();
-        foreach ($orders as $key => $order) {
-            $phone = $order->get_billing_phone();
-            $temp_array[] = $phone;
-        }
-
-        $shortdata = (array_unique($temp_array));
+        $selected = $_GET['dogl-names'];
+        $val = data_phone();
         //print_r($shortdata);
-        $shortcountdata = count($shortdata);
+        $shortcountdata = count($val);
         echo '<h2 >Total record : ' . $shortcountdata . '</h2>'; ?>
-        <a href='admin.php?page=all-smscampain&action=all-smscampain'> modify search </a>
-
-    <?php
-        // =====================================
-    }
-    ?>
-
-    <form action="admin.php?page=all-smscampain" method="POST">
-        <br>
-        <div>
+            <a href='admin.php?page=all-smscampain&action=all-smscampain'> modify search </a>
 
             <?php
+     
+    }
+    
+if (!empty($_GET['dogl-names'])) { ?>
+                <form action="" method="POST">
+                    <br>
+                    <div>
+
+                        <?php
 
             $username = smsalert_get_option('smsalert_name', 'smsalert_gateway');
             $password = smsalert_get_option('smsalert_password', 'smsalert_gateway');
@@ -77,8 +87,8 @@ global $shortdata;
 
             $cred = ($credits['description']['routes']);
             ?>
-            <div>
-                <br><select>
+                            <div>
+                                <br><select>
                     <?php
                     foreach ($cred as $key => $creditroot) {
                         $creditrout = $creditroot['route'];
@@ -91,9 +101,9 @@ global $shortdata;
                     ?>
 
                 </select>
-            </div>
-            <div>
-                <br><select>
+                            </div>
+                            <div>
+                                <br><select>
                     <?php
                     foreach ($senderids as $key => $senderid) {
                         $boards = $senderid['Senderid']['sender'];
@@ -107,31 +117,31 @@ global $shortdata;
                     ?>
 
                 </select>
-                    </div>
-            <select name="temp">
+                            </div>
+                            <select name="temp">
                 <option value="temp">slect templates</option>
-                <option value=" ">aslike</option>	
+                <option value=" ">aslike</option>
             </select> <br>
-            <textarea name="message" id="message" placeholder="Enter your message here"></textarea>
-        </div>
-        <input type="hidden" name="dogl-names" value="<?php echo $selected ;?>">
+                            <textarea name="message" id="message" placeholder="Enter your message here"></textarea>
+                    </div>
 
-        <input type="submit" name="submit" value="send_sms">
-    </form>
-<?php
+                    <input type="hidden" name="dogl-names" value="<?php echo $selected; ?>">
 
+                    <input type="submit" name="submit" value="send_sms">
+                </form>
+                <?php
+}
     if (!empty($_REQUEST['submit']) && ($_REQUEST['submit']) == 'send_sms') {
 
 
 
-
-
         $datas = array();
-        foreach($shortdata as $newval) {
-            $datas[] = array('number' => $newval, 'sms_body' => $_GET['message']);
+        $val = data_phone();
+        foreach ($val as $newval) {
+            $datas[] = array('number' => $newval, 'sms_body' => $_POST['message']);
         }
-        print_r($datas);
-        exit();
+      echo 'send sms';
+      exit();
         $respo    = SmsAlertcURLOTP::send_sms_xml($datas);
         $response_arr = json_decode($respo, true);
     }

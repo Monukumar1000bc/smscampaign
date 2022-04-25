6840ae7762162a5ea3ffda73c75b0b737dff5f54
monukumar <monu@cozyvision.com>
2022-04-25 13:29:04 +0530

    complete smscampaign

--- helper/smscampain.php
+++ helper/smscampain.php
@@ -35,7 +35,7 @@ function subscriber_page_smscampain()
         }
         
         .ptomato {
-            color: tomato;
+            color: #504d4d;
             font-size: 1em;
             /* color: whitesmoke; */
             margin-bottom: 1em;
@@ -47,7 +47,7 @@ function subscriber_page_smscampain()
     </style>
 
     <div class="container wp-core-ui" style=" min-width: 960px; height: 100%;margin: 0 auto;padding: 1.5em;background-color: #F0F0F1;font-size: 1rem;font-family: 'Lato'; ">
-        <h1 style="font-size: 2em;color: whitesmoke;font-weight: 400;text-align: center;margin-bottom: 0.5em;margin-top: 0;border-radius: 4px;padding: 10px;background: rgb(248, 107, 82);width: 100%;">SMSCAMPAIGN</h1>
+        <h1 style="font-size: 2em;color: #504d4d;font-weight: 400;text-align: left;margin-bottom: 0.5em;margin-top: 0;border-radius: 4px;padding: 10px;background: #F0F0F1;width: 100%;">SMS CAMPAIGN</h1>
         <!-- SEARCH DATA -->
         <div style=" display: flex;flex-direction: column;flex-wrap: wrap;">
         <?php
@@ -69,7 +69,7 @@ function subscriber_page_smscampain()
                 </p>
                 <p class="ptomato">
                 <input type="hidden" name="page" value="all-smscampain">
-                <input type="submit" name="submit" value="Search data" style=" display: inline-block;padding: 8px 12px;color: white;background-color: tomato;border: 0;border-radius: 5px;cursor: pointer;transition: all 300ms ease;">
+                <input type="submit" name="submit" value="Search data" style=" display: inline-block;padding: 8px 12px;color: #ffff;background-color: #2271B1;border: 1px solid #F0F0F1;border-radius: 5px;cursor: pointer;transition: all 300ms ease;box-shadow: 0px 0px 0px 0px #2271b1eb;">
                 </p>
 
             </form>
@@ -81,7 +81,7 @@ function subscriber_page_smscampain()
         $val = data_phone();
         $shortcountdata = count($val);
         ?><div style="display:flex"><?php
-        echo '<h2 style ="color:tomato;">Total record : ' . $shortcountdata . '</h2>'; ?>
+        echo '<h3 style ="color:#504d4d;">Total record : ' . $shortcountdata . '</h3>'; ?>
         <a href='admin.php?page=all-smscampain&action=all-smscampain' style="margin-top:20px; margin-left:10px;"> modify search </a>
           </div>
         <?php
@@ -97,6 +97,8 @@ function subscriber_page_smscampain()
 
             $username = smsalert_get_option('smsalert_name', 'smsalert_gateway');
             $password = smsalert_get_option('smsalert_password', 'smsalert_gateway');
+            $result    = SmsAlertcURLOTP::get_templates( $username, $password );
+            $templates = (array)json_decode( $result, true );
             //   SmsAlertcURLOTP::sendsms($shortdata);
             $result = SmsAlertcURLOTP::get_senderids($username, $password);
             $arr = json_decode($result, true);
@@ -146,24 +148,33 @@ function subscriber_page_smscampain()
                     <p class="ptomato">
                         <label>
                         Templates:<br>
-                        <select style=" color: inherit;font: inherit;margin: 0;margin-top: 0.5em;width: 100% !important;">
-                            <option value="" disabled selected>Select...</option>
-                            <option>This is option 1</option>
-                            <option>This is option 2</option>
-                            <option>This is option 3</option>
-                        </select>
+                        
+                            
+                    <select name="smsalert_templates" id="smsalert_templates" style="width:87%;color: inherit;font: inherit;margin: 0;margin-top: 0.5em;width: 100% !important;" onchange="return selecttemplate(this, '#wc_sms_alert_sms_order_message');">
+                            <option value=""><?php esc_html_e( 'Select Template', 'sms-alert' ); ?></option>
+						<?php
+						if ( array_key_exists( 'description', $templates ) && ( ! array_key_exists( 'desc', $templates['description'] ) ) ) {
+							foreach ( $templates['description'] as $template ) {
+								?>
+						<option value="<?php echo esc_textarea( $template['Smstemplate']['template'] ); ?>"><?php echo esc_attr( $template['Smstemplate']['title'] ); ?></option>
+								<?php
+							}
+						}
+						?>
+						</select>
+                       
                     </label>
                     </p>
 
                     <p class="ptomato">
                         <label>
                         SMS Text:<br>
-                        <textarea name="message" style=" color: inherit;font: inherit;margin: 0;margin-top: 0.5em; width:40%;" rows="5" cols="40" placeholder="Your message. I'm afraid I still don't understand, sir. Maybe if we felt any human loss as keenly as we feel one of those close to us, human history would be far less bloody."></textarea>
+                        <textarea name="message" id="wc_sms_alert_sms_order_message" style=" color: inherit;font: inherit;margin: 0;margin-top: 0.5em; width:40%;" rows="5" cols="40" placeholder="Your message. I'm afraid I still don't understand, sir. Maybe if we felt any human loss as keenly as we feel one of those close to us, human history would be far less bloody."></textarea>
                     </label>
                     </p>
                     <p class="ptomato">
                         <input type="hidden" name="dogl-names" value="<?php echo $selected; ?>"> 
-                        <input type="submit" name="submit" value="Send SMS" style=" display: inline-block;padding: 6px 10px;color: white;font: inherit;background-color: tomato;border: 0;border-radius: 5px;cursor: pointer;transition: all 300ms ease;">
+                        <input type="submit" name="submit" value="Send SMS" style=" display: inline-block;padding: 6px 10px;color: #ffff;font: inherit;background-color: #2271B1;border: 1px solid #F0F0F1;border-radius: 5px;cursor: pointer;transition: all 300ms ease;box-shadow: 0px 0px 0px 0px #2271b1eb;">
                     </p>
                 </div>
             </form>
